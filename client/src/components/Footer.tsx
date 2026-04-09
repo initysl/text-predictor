0a74e34809f7ede7a12c015115af50a87abc23b7
Yusuf Lawal <lawalyusufimp@gmail.com>
2026-04-09 16:41:22 +0100

    fix: icons

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -5,7 +5,7 @@ import { FaXTwitter } from 'react-icons/fa6';
 const Footer: React.FC = () => {
   return (
     <footer className='bg-white border-t border-gray-200'>
-      <div className='max-w-4xl mx-auto px-4 py-5 flex justify-center flex-wrap items-center gap-3 text-sm text-gray-600'>
+      <div className='max-w-4xl mx-auto px-4 py-5 flex justify-center flex-col items-center gap-3 text-sm text-gray-600'>
         <p>Trained on WikiText-103</p>
         <div>
           <a
