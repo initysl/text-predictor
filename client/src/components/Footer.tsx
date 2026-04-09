c8c6564a2fa6cb2be92d9bc19d6162245c2b0e2a
Yusuf Lawal <lawalyusufimp@gmail.com>
2026-04-09 16:14:17 +0100

    fix: icons

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,30 +1,31 @@
 import React from 'react';
-import { FaGithubSquare } from 'react-icons/fa';
-import { FaSquareXTwitter } from 'react-icons/fa6';
+import { FaGithub } from 'react-icons/fa';
+import { FaXTwitter } from 'react-icons/fa6';
 
 const Footer: React.FC = () => {
   return (
     <footer className='bg-white border-t border-gray-200'>
-      <div className='max-w-4xl mx-auto px-4 py-5 flex justify-center flex-col sm:flex-row items-center gap-3 text-sm text-gray-600'>
+      <div className='max-w-4xl mx-auto px-4 py-5 flex justify-center flex-wrap items-center gap-3 text-sm text-gray-600'>
         <p>Trained on WikiText-103</p>
+        <div>
+          <a
+            href='https://github.com/initysl/text-predictor'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='flex items-center gap-2 hover:underline'
+          >
+            <FaGithub className='text-lg' />
+          </a>
 
-        <a
-          href='https://github.com/initysl/text-predictor'
-          target='_blank'
-          rel='noopener noreferrer'
-          className='flex items-center gap-2 hover:underline'
-        >
-          <FaGithubSquare className='text-lg' />
-        </a>
-
-        <a
-          href='https://x.com/initysl'
-          target='_blank'
-          rel='noopener noreferrer'
-          className='flex items-center gap-2 hover:underline'
-        >
-          <FaSquareXTwitter className='text-lg' />
-        </a>
+          <a
+            href='https://x.com/initysl'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='flex items-center gap-2 hover:underline'
+          >
+            <FaXTwitter className='text-lg' />
+          </a>
+        </div>
       </div>
     </footer>
   );
